54db748fd9675c9eee9d6a91873cee0532af4e70
Adam Nord <adam.nord@abbindustrigymnasium.se>
2021-05-10 22:13:09 +0200

    3

--- testspel/Assets/Scripts/GenerateTerrain.cs
+++ testspel/Assets/Scripts/GenerateTerrain.cs
@@ -7,17 +7,24 @@ using UnityEngine.Tilemaps;
 
 public class GenerateTerrain : MonoBehaviour
 {
-    public RuleTile tile;
-    public Tilemap tilemap;
+    public RuleTile groundRuletile;
+    public Tilemap groundTilemap;
+    public Tile oreTile;
+    public Tilemap oreTilemap;
     public float scale;
     public float amplitude;
-    public int chunkSize = 128;
+    public int worldSize = 1024;
+
+    [Range(0.0f, 1.0f)]
+    public float rarity;
     void Start() {
-        for(int x = -128; x <= 128; x++) {
-            float xFloat = (float)x / chunkSize * amplitude;
+        for(int x = -worldSize; x <= worldSize; x++) {
+            float xFloat = (float)x / worldSize * amplitude;
             float height = Mathf.PerlinNoise(xFloat, 0.0f) * scale;
             for(int y = (int) height; y > -100; y--) {
-                tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                if (y < 10 && Mathf.PerlinNoise(xFloat, y / (height + 100) * 16) > rarity) oreTilemap.SetTile(new Vector3Int(x, y, 0), oreTile);
+                
+                groundTilemap.SetTile(new Vector3Int(x, y, 0), groundRuletile);
             }
         }
     }
